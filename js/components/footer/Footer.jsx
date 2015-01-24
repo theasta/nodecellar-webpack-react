886944840a14a261392412f55f3421d86e26a3e3
Alexandrine Boissière <theasta@gmail.com>
2015-01-23 23:22:55 -0800

    Add link to the blog

--- js/components/footer/Footer.jsx
+++ js/components/footer/Footer.jsx
@@ -11,7 +11,7 @@ module.exports = React.createClass({
                 <p>Built as a sample application with <a href="http://facebook.github.io/react/">React</a>,&nbsp;
                     <a href="http://facebook.github.io/flux/">Flux</a> and&nbsp;
                     <a href="http://twitter.github.com/bootstrap/">Twitter Bootstrap</a>&nbsp;
-                    by Alexandrine Boissière.
+                    by <a href={conf.BLOG_URL} target="_blank">Alexandrine Boissière</a>.
                     <br/>
                     The source code for this application is available on <a href={conf.GITHUB_REPO}>GitHub</a>.</p>
                 <p>Nodecellar was originally created by <a href={conf.ORIGINAL_GITHUB_REPO} target="_blank">Christophe Coenraets</a>.</p>
